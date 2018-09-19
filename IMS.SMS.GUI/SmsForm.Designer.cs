c9e5ce1056322a69df379b19a13fd190396d993e
Dymytr Yovchev <Dymytr.Yovchev@simcorp.com>
2018-09-19 17:34:39 +0300

    Lab3

--- IMS.SMS.GUI/SmsForm.Designer.cs
+++ IMS.SMS.GUI/SmsForm.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace IMS.SMS.GUI {
+﻿using System;
+
+namespace IMS.SMS.GUI {
     partial class SmsForm {
         /// <summary>
         /// Required designer variable.
@@ -23,23 +25,91 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.FormattingComboBox = new System.Windows.Forms.ComboBox();
+            this.MessageBox = new System.Windows.Forms.RichTextBox();
+            this.SMSProvider = new SmsProvider(new SmsProvider.SMSReceivedDelegate(OnSmsReceived));
             this.SuspendLayout();
             // 
-            // Form1
+            // FormattingComboBox
+            // 
+            this.FormattingComboBox.FormattingEnabled = true;
+            this.FormattingComboBox.Items.AddRange(new object[] {
+            "None",
+            "Start with DateTime",
+            "End with DateTime",
+            "Lowercase",
+            "Uppercase"});
+            this.FormattingComboBox.Location = new System.Drawing.Point(30, 38);
+            this.FormattingComboBox.Name = "FormattingComboBox";
+            this.FormattingComboBox.Size = new System.Drawing.Size(236, 21);
+            this.FormattingComboBox.TabIndex = 0;
+            this.FormattingComboBox.Text = "Select Formatting";
+            this.FormattingComboBox.SelectedIndexChanged += new System.EventHandler(this.FormattingComboBox_SelectedIndexChanged);
+            // 
+            // MessageBox
+            // 
+            this.MessageBox.Location = new System.Drawing.Point(30, 79);
+            this.MessageBox.Name = "MessageBox";
+            this.MessageBox.Size = new System.Drawing.Size(451, 429);
+            this.MessageBox.TabIndex = 1;
+            this.MessageBox.Text = "";
+            // 
+            // SmsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(510, 532);
+            this.Controls.Add(this.MessageBox);
+            this.Controls.Add(this.FormattingComboBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "Form1";
+            this.Name = "SmsForm";
+            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "SmsProvider";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox FormattingComboBox;
+        private System.Windows.Forms.RichTextBox MessageBox;
+        private SmsProvider SMSProvider;
+        readonly object someEventLock = new object();
+
+
+        public event Func<string, string> Formatter;
+
+        private string FormatWithDateTimeOnStart(string message) {
+            return  $"[{DateTime.Now}] {message}";
+        }
+
+        private string FormatWithDateTimeOnEnd(string message) {
+            return  $"{message} [{DateTime.Now}] ";
+        }
+
+        private static string FormatUpperCase(string message) {
+            return message.ToUpper();
+        }
+
+        private static string FormatLowerCase(string message) {
+            return message.ToLower();
+        }
+
+        public void OnSmsReceived(string msg) {
+            lock (someEventLock) {
+
+                if (MessageBox.InvokeRequired == true) {
+                    this?.BeginInvoke(new SmsProvider.SMSReceivedDelegate(OnSmsReceived), msg);
+                }
+                else {
+                    msg = Formatter?.Invoke(msg) ?? msg;
+                    MessageBox.AppendText($"{msg}{Environment.NewLine}");
+                }
+            }
+        }
     }
 }
 
---
+++ IMS.SMS.GUI/SmsProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMS.SMS.GUI {
+    public class SmsProvider {
+        public delegate void SMSReceivedDelegate(string message);
+        public event SMSReceivedDelegate SMSReceived;
+
+        private static int SmsCount { get; set; } = 0;
+        private Timer GenerateMsgTimer;
+
+        public SmsProvider(SMSReceivedDelegate del ) {
+            SMSReceived += del;
+            GenerateMsgTimer = new Timer();
+            GenerateMsgTimer.Tick += OnSmsGenerated;
+            GenerateMsgTimer.Interval = 1000;
+            GenerateMsgTimer.Start();
+        }
+
+        public void OnSmsGenerated(object o,EventArgs e) {
+            var message = "DRYV";
+            SmsCount += 1;
+            SMSReceived?.BeginInvoke( $"Message #{SmsCount} received: " + message, null, null);
+        }
+    }
+
+}
