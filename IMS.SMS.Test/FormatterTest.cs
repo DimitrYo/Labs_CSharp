087b346bb0218b3eca1ca073c84d6bf5d593c48c
Dymytr Yovchev <Dymytr.Yovchev@simcorp.com>
2018-09-21 15:14:47 +0300

    Lab3 finished.

--- IMS.SMS.Test/FormatterTest.cs
+++ IMS.SMS.Test/FormatterTest.cs
@@ -20,27 +20,18 @@ namespace IMS.SMS.Test {
         }
 
         public void FormatWithDateTimeOnEndTest() {
-            //
-            // TODO: Add test logic here
-            //
             var expected = ']';
             var actual = Formatter.FormatWithDateTimeOnEnd(msg);
             Assert.AreEqual(expected, actual[actual.Length - 1]);
         }
 
         public void FormatUpperCase() {
-            //
-            // TODO: Add test logic here
-            //
             var expected = 'T';
             var actual = Formatter.FormatUpperCase(msg);
             Assert.AreEqual(expected, actual[0]);
         }
 
         public void FormatLowerCase() {
-            //
-            // TODO: Add test logic here
-            //
             var expected = 'E';
             var actual = Formatter.FormatLowerCase(msg);
             Assert.AreEqual(expected, actual[1]);
